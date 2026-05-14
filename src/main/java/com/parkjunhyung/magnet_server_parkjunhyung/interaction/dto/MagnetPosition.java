63ae07b995e1dcea81e6e45ce07b349298e03812
hignandlight <junhyunghl126@gmail.com>
2026-05-15 00:11:09 +0900

    feat : Allow multi-character magnet letters(ex. "COF", "FEE")

--- src/main/java/com/parkjunhyung/magnet_server_parkjunhyung/interaction/dto/MagnetPosition.java
+++ src/main/java/com/parkjunhyung/magnet_server_parkjunhyung/interaction/dto/MagnetPosition.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record MagnetPosition(
-        @NotBlank @Size(min = 1, max = 1) String letter,
+        @NotBlank @Size(min = 1, max = 10) String letter, //자석 "COF", "FEE"식으로 오는 경우 고려
         @NotNull Double x,
         @NotNull Double y
 ) {
